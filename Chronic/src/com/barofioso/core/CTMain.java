341d322fb21d1b55ce10965aa403adafe63a6fb3
Barofioso <barofioso@chronic-miner.com>
2013-07-09 19:53:37 +0200

    Some more Standard Stuff

--- Chronic/src/com/barofioso/core/CTMain.java
+++ Chronic/src/com/barofioso/core/CTMain.java
@@ -1,7 +1,17 @@
 package com.barofioso.core;
 
+import net.minecraft.creativetab.CreativeTabs;
+
+import com.barofioso.core.creativeTabs.TabCTMain;
+import com.barofioso.core.creativeTabs.TabCTMachines;
+import com.barofioso.core.creativeTabs.TabCTOres;
+import com.barofioso.core.handler.CTFuelHandler;
+import com.barofioso.core.handler.LocalizationHandler;
+import com.barofioso.core.init.ModBlocks;
+import com.barofioso.core.init.Recipes;
 import com.barofioso.core.lib.Reference;
 import com.barofioso.core.proxies.CommonProxy;
+import com.barofioso.core.world.CTWorldGenerator;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
@@ -23,18 +33,37 @@ public class CTMain {
 	@Instance("ChronicTech")
 	public static CTMain instance;
 	
+	CTFuelHandler fuelhandler = new CTFuelHandler();
+	CTWorldGenerator worldGenerator = new CTWorldGenerator();
+	
+	
 	@SidedProxy(clientSide = Reference.CLIENT_SIDE_LOCATION, serverSide = Reference.SERVER_SIDE_LOCATION)
 	public static CommonProxy proxy;
+	
+	public static CreativeTabs TabCTMain = new TabCTMain(CreativeTabs.getNextID(), Reference.TAB_MAIN);
+	public static CreativeTabs TabCTMachines = new TabCTMachines(CreativeTabs.getNextID(), Reference.TAB_MACHINES);
+	public static CreativeTabs TabCTOre = new TabCTOres(CreativeTabs.getNextID(), Reference.TAB_ORES);
+	
 	
 	@PreInit
 	public void preInit(FMLPreInitializationEvent event) {
 		
+		LocalizationHandler.loadLanguages();
+		ModBlocks.Init();
+		Recipes.Init();
 	}
+	
+	
+	
 	
 	@Init
 	public void init(FMLInitializationEvent event) {
 		
+
 	}
+	
+	
+	
 	
 	@PostInit
 	public static void postInit(FMLPostInitializationEvent event) {
--- Chronic/src/com/barofioso/core/lib/Reference.java
+++ Chronic/src/com/barofioso/core/lib/Reference.java
@@ -2,10 +2,15 @@ package com.barofioso.core.lib;
 
 public class Reference {
 	
-	public static final String MOD_ID = "Chronic";
-	public static final String MOD_NAME = "Chronic";
+	public static final String MOD_ID = "chronicTech";
+	public static final String MOD_NAME = "Chronic Tech";
 	public static final String VERSION_NR = "0.0.1";
 	public static final String SERVER_SIDE_LOCATION = "com.barofioso.core.proxies.CommonProxy";
 	public static final String CLIENT_SIDE_LOCATION = "com.barofioso.core.proxies.ClientProxy";
+	
+	//Tabs
+	public static final String TAB_MAIN = "TabCTMain";
+	public static final String TAB_MACHINES = "TabCTMachines";
+	public static final String TAB_ORES = "TabCTOres";
 	
 }
