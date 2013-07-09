bed9290b2229cedbe5e2cd05ba585929508f1eb1
Barofioso <barofioso@chronic-miner.com>
2013-07-09 21:14:14 +0200

    Experimental Fuel Handler implementation or Base class for next ideas :D

--- Chronic/src/com/barofioso/core/handler/CTFuelHandler.java
+++ Chronic/src/com/barofioso/core/handler/CTFuelHandler.java
@@ -1,5 +1,8 @@
 package com.barofioso.core.handler;
 
+import com.barofioso.core.ItemBase;
+import com.barofioso.core.init.ModItems;
+
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.IFuelHandler;
 
@@ -8,14 +11,14 @@ public class CTFuelHandler implements IFuelHandler {
 	@Override
 	public int getBurnTime(ItemStack fuel) {
 		
-		switch(fuel.itemID){
-		
-		case ModItems.BlackStick.itemID: {return 2400;}
-		
-		
-		
-		default: return 0;
+		if(fuel.itemID == ModItems.copperIngot.itemID){
+			return 2400;
 		}
+		else{
+		return 0;
+	
+		}
+		
 	}
 
 }
